bbc118929fd8298444f40926c2c5393f86297f55
Quesada <tqaw19@gmail.com>
2018-09-07 17:37:56 -0500

    clases y entradas

--- src/main/java/com/tecsup/lab01/view/UserView.java
+++ src/main/java/com/tecsup/lab01/view/UserView.java
@@ -1,5 +1,14 @@
 package com.tecsup.lab01.view;
-
+import javax.swing.*;
+import com.tecsup.lab01.controller.*;
 public class UserView {
-    
+	 public static void main( String[] args )
+	    {
+		 
+		 
+	       JOptionPane.showMessageDialog(null, "Nombre completo: " + nombres);
+	       JOptionPane.showMessageDialog(null, "Edad: "+ edad);
+	       
+	       
+	    }
 }
