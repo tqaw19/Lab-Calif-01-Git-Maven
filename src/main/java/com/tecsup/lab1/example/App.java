c39d0dcbed646cb5413cc4a91f48f922eb7e45e7
Quesada <tqaw19@gmail.com>
2018-09-07 15:51:06 -0500

    primer commit

---
+++ src/main/java/com/tecsup/lab1/example/App.java
@@ -0,0 +1,13 @@
+package com.tecsup.lab1.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hola mundo LAB CALIFICADO!" );
+    }
+}
